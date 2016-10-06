3336dd22622cd950fab0baf66d7c3d2831136ad5
zbx-sadman <sadman41@ya.ru>
2016-10-06 15:55:56 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# WMI
-Various WMI stuff and templates
+#WMI Helper 
+
+This is a little Powershell script that helps to use WMI with Zabbix
+
+Actual release 0.9.0
+
+Tested on:
+- Production mode: Windows Server 2008 R2 SP1, WSUS 3.0 SP1, Powershell 2;
+- Non-production mode: Windows Server 2012 R2, WSUS 6, Powershell 4.
+
+Actions:
+- _Discovery_ - Make Zabbix's LLD JSON from WMI instances, which fetched with WMI query;
+- _Get_       - Get value of WMI object or array item;
+
+
+###How to use standalone
+
+        # Show value of first item of OperationalStatus array that fetched from HP_Processor class of ROOT\HPQ namespace.
+        # Verbose output is enabled.
+        powershell.exe -NoProfile -ExecutionPolicy "RemoteSigned" -File "wmi_helper.ps1" -Action "Get" -Namespace "ROOT\HPQ" -Query "select OperationalStatus from HP_Processor where DeviceID='Proc 2'" -Idx "0" -Verbose -defaultConsoleWidth 
+
+
+###How to use with Zabbix
+1. Just include [zbx\_wmi\_helper.conf](https://github.com/zbx-sadman/wmi/tree/master/Zabbix_Templates/zbx_wmi_helper.conf) to Zabbix Agent config;
+2. Put _wmi\_helper.ps1_ to _C:\zabbix\scripts_ dir. If you want to place script to other directory, you must edit _zbx\_wmi\_helper.conf_ to properly set script's path; 
+3. Set Zabbix Agent's / Server's _Timeout_ to more that 3 sec (may be 10 or 30);
+4. Import [template(s)](https://github.com/zbx-sadman/wsus_miner/tree/master/Zabbix_Templates) to Zabbix Server;
+5. Be sure that Zabbix Agent worked in Active mode - in template can be used 'Zabbix agent(active)' poller type. Otherwise - change its to 'Zabbix agent' and increase value of server's StartPollers parameter;
+6. Enjoy.
+
+**Note #1**
+Do not try import Zabbix v2.4 template to Zabbix _pre_ v2.4. You need to edit .xml file and make some changes at discovery_rule - filter tags area and change _#_ to _<>_ in trigger expressions. I will try to make template to old Zabbix.
+
+**Note #2**
+Be sure that all doublequotes is escaped, if its used in query string with Zabbix. Otherwise you will see error messages instead expected values.
+
+###Hints
+- You can use WMI Helper to bring values '1' or '0' instead Windows's 'True' or 'False' to Zabbix's Data Item. Example: `wmi.helper[get,root\hpq,select PoweredOn from HP_ProcessorChip where Name=\"Proc 1\"]`;
+- To get on Zabbix Server side properly UTF-8 output when have non-english (for example Russian Cyrillic) symbols in Computer Group's names, use  _-consoleCP **your_native_codepage**_ command line option. For example to convert from Russian Cyrillic codepage (CP866): _... "wsus_miner.ps1"  ... -consoleCP CP866_;
+- If u need additional symbol escaping in LLD JSON - just add one or more symbols to _$EscapedSymbols_ array in _PrepareTo-Zabbix_ function;
+- To measure script runtime use _Verbose_ command line switch,
+
+Beware: frequent connections to WSUS may be nuke host server, make over 9000% CPU utilization and yours requests will be processeed slowly. To avoid it - don't use small update intervals with Zabbix's Data Items and disable unused.
---
+++ wmi_helper.ps1
@@ -0,0 +1,268 @@
+<#                                          
+    .SYNOPSIS  
+        Return instances of HP Insight Management WBEM objects as LLD-JSON for Zabbix
+        Also returns value of specified by index item of array that fetched by WMI query
+
+    .DESCRIPTION
+        Return instances of HP Insight Management WBEM objects as LLD-JSON for Zabbix
+        Also returns value of specified by index item of array that fetched by WMI query
+
+    .NOTES  
+        Version: 0.9.0
+        Name: WMI helper
+        Author: zbx.sadman@gmail.com
+        DateCreated: 
+        Testing environment: Windows Server 2008R2 SP1, Powershell 2.0
+        Non-production testing environment: Windows Server 2012 R2, PowerShell 4
+
+    .LINK  
+        https://github.com/zbx-sadman
+
+    .PARAMETER Action
+        What need to do with WMI objects :
+            Discovery - Make Zabbix's LLD JSON;
+            Get       - Get value of object or array item.
+
+    .PARAMETER Namespace
+        Namespace to WMI query
+
+    .PARAMETER Query
+        WMI Query
+
+    .PARAMETER Idx
+        Array index
+
+    .PARAMETER ConsoleCP
+        Codepage of Windows console. Need to properly convert output to UTF-8
+
+    .PARAMETER DefaultConsoleWidth
+        Say to leave default console width and not grow its to $CONSOLE_WIDTH
+
+    .PARAMETER Verbose
+        Enable verbose messages
+
+    .EXAMPLE 
+        powershell.exe -NoProfile -ExecutionPolicy "RemoteSigned" -File "wmi_helper.ps1" -Action "Get" -Namespace "ROOT\HPQ" -Query "select OperationalStatus from HP_Processor where DeviceID='Proc 2'" -Idx "0" -Verbose -defaultConsoleWidth 
+
+        Description
+        -----------  
+        Return Value of first item of OperationalStatus array that fetched from HP_Processor class of ROOT\HPQ namespace.
+        Verbose messages is enabled.
+
+
+#>
+
+
+Param (
+   [Parameter(Mandatory = $False)] 
+   [ValidateSet('Discovery', 'Get')]
+   [string]$Action,
+   [Parameter(Mandatory = $True)]
+   [String]$Namespace,
+   [Parameter(Mandatory = $True)]
+   [String]$Query,
+   [Parameter(Mandatory = $False)]
+   [String]$Idx,
+   [Parameter(Mandatory = $False)]
+   [String]$ErrorCode,
+   [Parameter(Mandatory = $False)]
+   [String]$ConsoleCP,
+   [Parameter(Mandatory = $False)]
+   [Switch]$DefaultConsoleWidth
+);
+
+#Set-StrictMode -Version Latest
+
+# Set US locale to properly formatting float numbers while converting to string
+[System.Threading.Thread]::CurrentThread.CurrentCulture = "en-US"
+# Width of console to stop breaking JSON lines
+Set-Variable -Option Constant -Name "CONSOLE_WIDTH" -Value 512
+
+####################################################################################################################################
+#
+#                                                  Function block
+#    
+####################################################################################################################################
+#
+#  Prepare string to using with Zabbix 
+#
+Function PrepareTo-Zabbix {
+   Param (
+      [Parameter(ValueFromPipeline = $True)] 
+      [PSObject]$InputObject,
+      [String]$ErrorCode,
+      [Switch]$NoEscape,
+      [Switch]$JSONCompatible
+   );
+   Begin {
+      # Add here more symbols to escaping if you need
+      $EscapedSymbols = @('\', '"');
+      $UnixEpoch = Get-Date -Date "01/01/1970";
+   }
+   Process {
+      # Do something with all objects (non-pipelined input case)  
+      ForEach ($Object in $InputObject) { 
+         If ($Null -Eq $Object) {
+           # Put empty string or $ErrorCode to output  
+           If ($ErrorCode) { $ErrorCode } Else { "" }
+           Continue;
+         }
+         # Need add doublequote around string for other objects when JSON compatible output requested?
+         $DoQuote = $False;
+         Switch (($Object.GetType()).FullName) {
+            'System.Boolean'  { $Object = [int]$Object; }
+            'System.DateTime' { $Object = (New-TimeSpan -Start $UnixEpoch -End $Object).TotalSeconds; }
+            Default           { $DoQuote = $True; }
+         }
+         # Normalize String object
+         $Object = $( If ($JSONCompatible) { $Object.ToString().Trim() } else { Out-String -InputObject (Format-List -InputObject $Object -Property *)});
+
+         If (!$NoEscape) { 
+            ForEach ($Symbol in $EscapedSymbols) { 
+               $Object = $Object.Replace($Symbol, "\$Symbol");
+            }
+         }
+
+         # Doublequote object if adherence to JSON standart requested
+         If ($JSONCompatible -And $DoQuote) { 
+            "`"$Object`"";
+         } else {
+            $Object;
+         }
+      }
+   }
+}
+
+#
+#  Convert incoming object's content to UTF-8
+#
+Function ConvertTo-Encoding ([String]$From, [String]$To){  
+   Begin   {  
+      $encFrom = [System.Text.Encoding]::GetEncoding($from)  
+      $encTo = [System.Text.Encoding]::GetEncoding($to)  
+   }  
+   Process {  
+      $bytes = $encTo.GetBytes($_)  
+      $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)  
+      $encTo.GetString($bytes)  
+   }  
+}
+
+#
+#  Make & return JSON, due PoSh 2.0 haven't Covert-ToJSON
+#
+Function Make-JSON {
+   Param (
+      [Parameter(ValueFromPipeline = $True)] 
+      [PSObject]$InputObject, 
+      [array]$ObjectProperties, 
+      [Switch]$Pretty
+   ); 
+   Begin   {
+      [String]$Result = "";
+      # Pretty json contain spaces, tabs and new-lines
+      If ($Pretty) { $CRLF = "`n"; $Tab = "    "; $Space = " "; } Else { $CRLF = $Tab = $Space = ""; }
+      # Init JSON-string $InObject
+      $Result += "{$CRLF$Space`"data`":[$CRLF";
+      # Take each Item from $InObject, get Properties that equal $ObjectProperties items and make JSON from its
+      $itFirstObject = $True;
+   } 
+   Process {
+      # Do something with all objects (non-pipelined input case)  
+      ForEach ($Object in $InputObject) {
+         # Skip object when its $Null
+         If ($Null -Eq $Object) { Continue; }
+
+         If (-Not $itFirstObject) { $Result += ",$CRLF"; }
+         $itFirstObject=$False;
+         $Result += "$Tab$Tab{$Space"; 
+         $itFirstProperty = $True;
+         # Process properties. No comma printed after last item
+         ForEach ($Property in $ObjectProperties) {
+            If (-Not $itFirstProperty) { $Result += ",$Space" }
+            $itFirstProperty = $False;
+            $Result += "`"{#$Property}`":$(PrepareTo-Zabbix -InputObject $Object.$Property -JSONCompatible)";
+         }
+         # No comma printed after last string
+         $Result += "$Space}";
+      }
+   }
+   End {
+      # Finalize and return JSON
+      "$Result$CRLF$Tab]$CRLF}";
+   }
+}
+
+$Result = 0;
+If ([string]::IsNullOrEmpty($Query)) { 
+   Write-Verbose "$(Get-Date) No query given";
+   exit; 
+}
+
+#If ([string]::IsNullOrEmpty($NameSpace)) { $NameSpace = "ROOT\HPQ"; }
+
+If ([string]::IsNullOrEmpty($Idx)) { $Idx = 0; }
+
+
+Write-Verbose "$(Get-Date) Taking instances...";
+
+# Prepare object lists
+
+$Objects = Get-WmiObject -Computer "." -Query $Query -NameSpace $NameSpace;
+
+#$Objects | fl *
+#exit
+
+Write-Verbose "$(Get-Date) Object(s) fetched, begin processing its with action: '$Action'";
+$Result = $(
+   # if no object in collection: 1) JSON must be empty; 2) 'Get' must be able to return ErrorCode
+   Switch ($Action) {
+      'Discovery' {
+         # Discovery given object, make json for zabbix
+         $Class = $Objects[0].__CLASS;
+         Write-Verbose "$(Get-Date) Class of object(s) is '$Class'";
+         $ObjectProperties = $(
+            Switch ($Class) {
+              #  'HP_DiskDrive' { @("DEVICEID", "NAME"); }
+              #  'HP_Processor' { @("DEVICEID", "NAME"); }
+              'HP_MemoryModule'         { @("NAME", "MANUFACTURER", "TAG"); }
+              # HP_EthernetPort is have HP_WinEthernetPort in real
+              'HP_WinEthernetPort'      { @("DEVICEID", "CAPTION", "PORTTYPE"); }
+              'HPSA_ArrayController'    { @("NAME", "ELEMENTNAME"); }
+              Default                   { @("DEVICEID", "NAME"); }
+            }  
+         )
+         Write-Verbose "$(Get-Date) Generating LLD JSON";
+         Make-JSON -InputObject $Objects -ObjectProperties $ObjectProperties -Pretty;
+      }
+      'Get' {
+         # Select value if single metric or array's item
+         If ($Null -ne $Objects) { 
+            Write-Verbose "$(Get-Date) Select value of single object or array item that selected by WMI query";
+            $Objects = $( If ($($Objects.Properties).IsArray) {
+                             $($Objects.Properties).Value[$Idx];
+                          } else {
+                             $($Objects.Properties).Value;
+                         }
+            );
+         }
+         PrepareTo-Zabbix -InputObject $Objects -ErrorCode $ErrorCode;
+      }
+   }
+);
+
+# Convert string to UTF-8 if need (For Zabbix LLD-JSON with Cyrillic chars for example)
+if ($consoleCP) { 
+   Write-Verbose "$(Get-Date) Converting output data to UTF-8";
+   $Result = $Result | ConvertTo-Encoding -From $consoleCP -To UTF-8; 
+}
+
+# Break lines on console output fix - increase console width to $CONSOLE_WIDTH chars
+if (!$defaultConsoleWidth) { 
+   Write-Verbose "$(Get-Date) Changing console width to $CONSOLE_WIDTH";
+   mode con cols=$CONSOLE_WIDTH; 
+}
+
+Write-Verbose "$(Get-Date) Finishing";
+
+$Result;
